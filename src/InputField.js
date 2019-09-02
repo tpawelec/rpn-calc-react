b638782ddef54e13373a2725d1e9750acc861868
Tomasz Pawelec <tpawelec@outlook.com>
2019-09-02 15:48:08 +0200

    Everything seems to be working. Changed string to array in state (easier to iterate and control over multidigit numbers). Have to add keyboard listening and parathensesis check. Deployed for beta testing

--- src/InputField.js
+++ src/InputField.js
@@ -4,7 +4,7 @@ class InputField extends Component {
 
     render() {
         return (
-            <input type="text" name={this.props.name} id={this.props.id} className={`calculator__input col-${this.props.col}`}  value={this.props.value}/>
+            <input readOnly type="text" name={this.props.name} id={this.props.id} className={`calculator__input col-${this.props.col}`}  value={this.props.value}/>
         )
     }
 }
