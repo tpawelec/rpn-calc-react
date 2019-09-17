2a2cc9c8bb532a66820f85dde2d275673ea6df35
Tomasz Pawelec <tpawelec@outlook.com>
2019-09-17 19:59:18 +0200

    Updates

--- precache-manifest.b88d42e9dffda0cd874e931e624adc43.js
+++ precache-manifest.b88d42e9dffda0cd874e931e624adc43.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69bc2d73f2264d51f067483f653f1285",
+    "revision": "173a7049d556dff3a311bb3265439b12",
     "url": "/rpn-calc-react/index.html"
   },
   {
-    "revision": "04c95aff322bda974b8b",
+    "revision": "144ca85e3b4f22705730",
     "url": "/rpn-calc-react/static/css/main.6d1436a9.chunk.css"
   },
   {
-    "revision": "30fda1af3d2d88fd33f9",
-    "url": "/rpn-calc-react/static/js/2.ade476dd.chunk.js"
+    "revision": "36e1f5feb6d3950ef624",
+    "url": "/rpn-calc-react/static/js/2.9a182ad4.chunk.js"
   },
   {
-    "revision": "04c95aff322bda974b8b",
-    "url": "/rpn-calc-react/static/js/main.0a3eb227.chunk.js"
+    "revision": "144ca85e3b4f22705730",
+    "url": "/rpn-calc-react/static/js/main.800c6dc4.chunk.js"
   },
   {
     "revision": "7e9453d8ce8c7cb1b727",
